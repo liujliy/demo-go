1e80856aaebe6912c840422d56f93cf5d22438c9
LIU.JIANG <liu.jiang234@iwhalecloud.com>
2021-05-14 16:00:35 +0800

    测试提交

--- mian.go
+++ mian.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image/color"
+)
 
 func main() {
-	fmt.Print("Hello Go!")
+	fmt.Println("Hello Go!")
+
+	// struct
+	test := ColorPoint{
+		Point: Point{3, 4},
+		Color: color.RGBA{G: 225, B: 225, A: 1},
+	}
+	fmt.Println(test)
 }
 
 type Point struct {
 	x, y float64
+}
+
+type ColorPoint struct {
+	Point
+	Color color.RGBA
 }
